f359ecc4c7bf0162ee9d89b7df04ce5cad07be19
Suchan An <me@ansuchan.com>
2016-11-03 15:57:38 +0900

    Add simple css change feature: select a theme from select element

--- popup.js
+++ popup.js
@@ -1,9 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-  chrome.tabs.insertCSS(
-    null,
-    {file: "themes/custom.css"},
-    function() {
-      console.log("simple theme loaded.");
-    }
-  );
+  var themesSelectElement = document.getElementById("themes");
+  themesSelectElement.addEventListener("change", function() {
+    var themeName = this.value;
+    var themeFilePath = "themes/" + themeName + ".css";
+
+    chrome.tabs.insertCSS(
+      null,
+      {file: themeFilePath},
+      function() {
+        console.log("simple theme loaded.");
+      }
+    );
+  });
 });
