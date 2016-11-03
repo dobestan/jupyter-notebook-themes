d5b00f0e951cda07a4ebe0439967259fe8d7832d
Suchan An <me@ansuchan.com>
2016-11-03 17:24:12 +0900

    Update popup.js: add custom insertCSS, removeCSS function

--- popup.js
+++ popup.js
@@ -1,15 +1,51 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var themesSelectElement = document.getElementById("themes");
-  themesSelectElement.addEventListener("change", function() {
-    var themeName = this.value;
+(function() {
+  var linkElementId = "jupyter-notebook-custom-css";
+
+
+  function insertCSS(themeName) {
     var themeFilePath = "themes/" + themeName + ".css";
 
-    chrome.tabs.insertCSS(
-      null,
-      {file: themeFilePath},
-      function() {
-        console.log("simple theme loaded.");
-      }
-    );
+    var linkElement = document.createElement("link");
+
+    linkElement.href = chrome.extension.getURL(themeFilePath);
+    linkElement.id = linkElementId;
+    linkElement.type = "text/css";
+    linkElement.rel = "stylesheet";
+
+    document.getElementsByTagName("head")[0].appendChild(linkElement);
+  }
+
+
+  function removeCSS() {
+    var linkElement = document.getElementById(linkElementId);
+    linkElement && linkElement.parentNode.removeChild(linkElement);
+  }
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var themesSelectElement = document.getElementById("themes");
+    themesSelectElement.addEventListener("change", function() {
+      var themeName = this.value;
+      insertCSS(themeName);
+      // chrome.tabs.insertCSS(
+      //   null,
+      //   {file: themeFilePath},
+      //   function() {
+      //     console.log("simple theme loaded.");
+      //   }
+      // );
+    });
+
+
+    var applyButtonElement = document.getElementsByName("apply")[0];
+    applyButtonElement.addEventListener("click", function() {
+      window.close();
+    });
+
+
+    var restoreButtonElement = document.getElementsByName("restore")[0];
+    restoreButtonElement.addEventListener("click", function() {
+      window.close();
+    });
   });
-});
+})();
